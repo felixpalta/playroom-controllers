3d305db8b16a1d1b2703ce6799c705ba16419b3e
Felix Palta <felix.palta@dune-hd.com>
2015-09-24 19:18:03 +0300

    lockbox: Change pin config.

--- lockbox-controller-A2/lockbox-controller-pin-config.h
+++ lockbox-controller-A2/lockbox-controller-pin-config.h
@@ -5,30 +5,30 @@ static const size_t LOCKBOX_PIN_NUMBER = 24;
 
 int lockbox_pins[LOCKBOX_PIN_NUMBER] =
 {
-	20, // Lockbox 0
-	21, // Lockbox 1
-	22, // Lockbox 2
-	23, // Lockbox 3
-	24, // Lockbox 4
-	25, // Lockbox 5
-	26, // Lockbox 6
-	27, // Lockbox 7
-	28, // Lockbox 8
-	29, // Lockbox 9
-	30, // Lockbox 10
-	31, // Lockbox 11
-	32, // Lockbox 12
-	33, // Lockbox 13
-	34, // Lockbox 14
-	35, // Lockbox 15
-	36, // Lockbox 16
-	37, // Lockbox 17
-	38, // Lockbox 18
-	39, // Lockbox 19
-	40, // Lockbox 20
-	41, // Lockbox 21
-	42, // Lockbox 22
-	43, // Lockbox 23
+	22, // Lockbox 1
+	23, // Lockbox 2
+	24, // Lockbox 3
+	25, // Lockbox 4
+	26, // Lockbox 5
+	27, // Lockbox 6
+	28, // Lockbox 7
+	29, // Lockbox 8
+	30, // Lockbox 9
+	31, // Lockbox 10
+	32, // Lockbox 11
+	33, // Lockbox 12
+	34, // Lockbox 13
+	35, // Lockbox 14
+	36, // Lockbox 15
+	37, // Lockbox 16
+	38, // Lockbox 17
+	39, // Lockbox 18
+	40, // Lockbox 19
+	41, // Lockbox 20
+	42, // Lockbox 21
+	43, // Lockbox 22
+	44, // Lockbox 23
+    45, // Lockbox 24
 };
 
 #endif // __LOCKBOX_CONTROLLER_PIN_CONFIG_H__
