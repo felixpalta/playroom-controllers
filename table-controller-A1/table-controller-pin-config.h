6901b6958ad097bb11f8f82a59361b8e24191ef0
Felix Palta <felix.palta@dune-hd.com>
2015-09-25 12:32:28 +0300

    table: Add sector pin config.

---
+++ table-controller-A1/table-controller-pin-config.h
@@ -0,0 +1,26 @@
+#ifndef __TABLE_CONTROLLER_PIN_CONFIG_H__
+#define __TABLE_CONTROLLER_PIN_CONFIG_H__
+
+static const size_t NUMBER_OF_SECTOR_PINS = 15;
+
+static const int sector_pins[NUMBER_OF_SECTOR_PINS] =
+{
+	3,	// Sector 1
+	4,	// Sector 2
+	5,	// Sector 3
+	6,	// Sector 4
+	7,	// Sector 5
+	8,	// Sector 6
+	9,	// Sector 7
+	10,	// Sector 8
+	11,	// Sector 9
+	12,	// Sector 10
+	// NOTE: Pin 13 should NOT be used.
+	14,	// Sector 11
+	15,	// Sector 12
+	16,	// Sector 13
+	17,	// Sector 14
+	18,	// Sector 15
+};
+
+#endif // __TABLE_CONTROLLER_PIN_CONFIG_H__
