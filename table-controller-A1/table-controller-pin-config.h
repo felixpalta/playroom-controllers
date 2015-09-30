2df31487354c3df0567563740df0def1c153f9df
Felix Palta <felix.palta@dune-hd.com>
2015-09-30 20:35:41 +0300

    table: Modified pin config table. Add funtions for led switching.

--- table-controller-A1/table-controller-pin-config.h
+++ table-controller-A1/table-controller-pin-config.h
@@ -1,32 +1,38 @@
 #ifndef __TABLE_CONTROLLER_PIN_CONFIG_H__
 #define __TABLE_CONTROLLER_PIN_CONFIG_H__
 
-static const size_t NUMBER_OF_SECTOR_PINS = 15;
+static const size_t NUMBER_OF_SECTORS = 15;
 
 // NOTE: If active level is LOW,
 // then external pull-down resistors should be connected to the pins.
 static const int SECTOR_PIN_ACTIVE_LEVEL = HIGH;
 
-static const int sector_pins[NUMBER_OF_SECTOR_PINS] =
+struct SectorPins
 {
-	3,	// Sector 1
-	4,	// Sector 2
-	5,	// Sector 3
-	6,	// Sector 4
-	7,	// Sector 5
-	8,	// Sector 6
-	9,	// Sector 7
-	10,	// Sector 8
-	11,	// Sector 9
-	12,	// Sector 10
-	// NOTE: Pin 13 should NOT be used.
-	14,	// Sector 11
-	15,	// Sector 12
-	16,	// Sector 13
-	17,	// Sector 14
-	18,	// Sector 15
+	int sensor_pin;
+	int number_led_pin;
+	int arrow_led_pin;
 };
 
+static const SectorPins sector_pins[NUMBER_OF_SECTORS] =
+{
+	/* sensor pin, number led pin, arrow led pin */
+	{ 3, 22, 23 }, // Sector 1
+	{ 4, 24, 25 }, // Sector 2
+	{ 5, 26, 27 }, // Sector 3
+	{ 6, 28, 29 }, // Sector 4
+	{ 7, 30, 31 }, // Sector 5
+	{ 8, 32, 33 }, // Sector 6
+	{ 9, 34, 35 }, // Sector 7
+	{ 10, 36, 37 }, // Sector 8
+	{ 11, 38, 39 }, // Sector 9
+	{ 12, 40, 41 }, // Sector 10
+	{ 14, 42, 43 }, // Sector 11
+	{ 15, 44, 45 }, // Sector 12
+	{ 16, 46, 47 }, // Sector 13
+	{ 17, 48, 49 }, // Sector 14
+	{ 18, 50, 51 }, // Sector 15
 
+};
 
 #endif // __TABLE_CONTROLLER_PIN_CONFIG_H__
