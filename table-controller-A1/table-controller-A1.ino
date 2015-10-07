f2218135b1d10beb4df571fd32b691c46ee6e78b
Felix Palta <felix.palta@dune-hd.com>
2015-10-07 18:25:53 +0300

    table: Add DBG LED blinking on sector motion callbacs.

--- table-controller-A1/table-controller-A1.ino
+++ table-controller-A1/table-controller-A1.ino
@@ -1,10 +1,13 @@
 #include "sectors.h"
 
+#define DBG_PIN 13
+
 void setup()
 {
 	Serial.begin(9600);
 	Serial.println("Hello from Table Controller");
 	sectors_init();
+	pinMode(DBG_PIN, OUTPUT);
 }
 
 void loop()
@@ -12,12 +15,25 @@ void loop()
 	sectors_process_sensors();
 }
 
+static void blink(int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		digitalWrite(DBG_PIN, HIGH);
+		delay(50);
+		digitalWrite(DBG_PIN, LOW);
+		delay(50);
+	}
+}
+
 void sectors_rotation_started_callback()
 {
 	Serial.println("ROTATION STARTED CALLBACK");
+	blink(2);
 }
 void sectors_rotation_stopped_callback(int sector_n)
 {
 	Serial.print("ROTATION STOPPED CALLBACK: ");
 	Serial.println(sector_n);
+	blink(sector_n);
 }
