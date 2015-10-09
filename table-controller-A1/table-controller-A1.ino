ca65ea059003f54f04c39d8218c55e6fa63a63bd
Felix Palta <felix.palta@dune-hd.com>
2015-10-09 20:41:45 +0300

    table: Add EthernetClient initialization.

--- table-controller-A1/table-controller-A1.ino
+++ table-controller-A1/table-controller-A1.ino
@@ -9,10 +9,30 @@
 
 #define DBG_PIN 13
 
+EthernetClient client;
+
+#define W5200_CS  10
+#define SDCARD_CS 4
+
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Hello from Table Controller");
+
+  pinMode(SDCARD_CS, OUTPUT);
+  digitalWrite(SDCARD_CS, HIGH);//Deselect the SD card
+
+  // start the Ethernet connection and the server:
+  if (!Ethernet.begin(table_controller_mac))
+  {
+    Serial.println("DHCP failed, trying to set IP manually...");
+    Ethernet.begin(table_controller_mac, table_controller_ip);
+  }
+
+  Serial.print("Table controller is at ");
+  Serial.print(Ethernet.localIP());
+  Serial.println(String(" : ") + table_controller_port);
+
   sectors_init();
   pinMode(DBG_PIN, OUTPUT);
   digitalWrite(DBG_PIN, LOW);
