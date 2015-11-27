a256513268a64531fac8147a2d2ea269995c5193
felix-at-playroom <fpalta90@gmai.com>
2015-11-28 00:05:23 +0300

    lockbox: add cleaning request to cleaning state

--- lockbox-controller-A2/playroomstatecleaning.cpp
+++ lockbox-controller-A2/playroomstatecleaning.cpp
@@ -3,6 +3,9 @@
 #include "curtain.h"
 #include "dimmers.h"
 #include "buttons.h"
+#include <RqSender.h>
+
+extern RqSender rq_sender;
 
 PlayroomStateCleaning::PlayroomStateCleaning(PlayroomStateMachineInterface &p)
     : state_machine(p)
@@ -11,6 +14,8 @@ PlayroomStateCleaning::PlayroomStateCleaning(PlayroomStateMachineInterface &p)
 
 void PlayroomStateCleaning::init()
 {
+  
+  rq_sender.send_request(OUT_RQ_TYPE_CLEANING, /*unused*/ 0);
   door_lock_open();
   curtain_signal_open();
   dimmers_light_set(DIMMER_TOP_LIGHT, 100);
