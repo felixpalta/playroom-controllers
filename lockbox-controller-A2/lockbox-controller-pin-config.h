3814564f85394bba7a3b47397cdf2367a53b0d47
felix-at-playroom <fpalta90@gmai.com>
2015-12-24 22:56:14 +0300

    lockbox: Change start game button light from pin 15 to pin 8.

--- lockbox-controller-A2/lockbox-controller-pin-config.h
+++ lockbox-controller-A2/lockbox-controller-pin-config.h
@@ -44,7 +44,7 @@ static const int GAME_START_BUTTON_PIN = 11;
 static const int STANDBY_BUTTON_PIN = 14;
 static const int CLEANING_BUTTON_PIN = 12;
 
-static const int GAME_START_BUTTON_LIGHT_PIN = 15;
+static const int GAME_START_BUTTON_LIGHT_PIN = 8;
 static const int STANDBY_BUTTON_LIGHT_PIN = 16;
 
 static const int DOOR_LOCK_PIN = 17;
