d2061c0b8627353db01092a52ca16b0b2e45285b
felix-at-playroom <fpalta90@gmai.com>
2016-06-22 21:58:36 +0300

    Add Heartbeat to table-controller

---
+++ table-controller-A1/heartbeat.h
@@ -0,0 +1,11 @@
+// heartbeat.h
+
+#ifndef _HEARTBEAT_h
+#define _HEARTBEAT_h
+
+void heart_beat_init(int pin);
+
+void heart_beat_process();
+
+#endif
+
--- table-controller-A1/table-controller-A1.ino
+++ table-controller-A1/table-controller-A1.ino
@@ -17,6 +17,7 @@
 #include <request_names.h>
 #include "request_processing.h"
 #include "MySerial.h"
+#include "heartbeat.h"
 
 EthernetClient client;
 RqSender rq_sender(client, MySerial, PROTOVER_ATTR_VALUE, SERIAL_ATTR_VALUE);
@@ -24,8 +25,7 @@ EthernetServer server(TABLE_CONTROLLER_LISTENING_PORT);
 
 void setup()
 {
-  pinMode(DBG_PIN, OUTPUT);
-  digitalWrite(DBG_PIN, LOW);
+  heart_beat_init(DBG_PIN);
   
   Serial.begin(115200);
   Serial.println("Hello from Table Controller");
@@ -73,6 +73,7 @@ void setup()
 
 void loop()
 {
+  heart_beat_process();
   process_serial_input();
   process_incoming_connections();
   SectorEventData sector_event_data;
@@ -183,23 +184,11 @@ static void disconnect_client(Client& client)
   MySerial.println("client disonnected");
 }
 
-static void blink(int n)
-{
-  for (int i = 0; i < n; ++i)
-  {
-    digitalWrite(DBG_PIN, HIGH);
-    delay(50);
-    digitalWrite(DBG_PIN, LOW);
-    delay(50);
-  }
-}
-
 void sectors_rotation_started_callback()
 {
   Serial.println("ROTATION STARTED CALLBACK");
 
   rq_sender.send_request(OUT_RQ_TYPE_BARREL_PLAY, /* not used */ 0);
-  blink(2);
 }
 void sectors_rotation_stopped_callback(int sector_n)
 {
@@ -207,5 +196,4 @@ void sectors_rotation_stopped_callback(int sector_n)
   Serial.println(sector_n);
   
   rq_sender.send_request(OUT_RQ_TYPE_BARREL_SECTOR, sector_n);
-  blink(sector_n);
 }
